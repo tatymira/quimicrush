d123243c1d378f36d747fc0c83c087a81fd0039f
tatymira <tatymmedeiros@gmail.com>
2019-06-22 14:00:09 -0300

    projeto Quimicrush

---
+++ Assets/Scripts/MudarCena.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MudarCena : MonoBehaviour {
+
+    public void ChangeToScene(string scene)
+    {
+        Application.LoadLevel(scene);
+    }
+}
